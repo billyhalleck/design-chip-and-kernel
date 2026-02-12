aca54308c4f3d9caf1a3804174329e81c1ecd99a
billyhalleck <billyhalleck@gmail.com>
2026-02-12 22:35:20 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,29 +1,40 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
 import cocotb
-from cocotb.triggers import Timer, RisingEdge
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
 
 @cocotb.test()
-async def test_mic1_initial(dut):
-    dut._log.info("Starting MIC-1 Robust Test")
+async def test_project(dut):
+    dut._log.info("Start")
 
-    cocotb.start_soon(cocotb.clock.Clock(dut.clk, 10, units="ns").start())
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())
 
- 
+    # Reset
+    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.rst_n.value = 0 # Reset aktif
-    
-  
-    for _ in range(5):
-        await RisingEdge(dut.clk)
-        
- 
-    dut._log.info("Releasing Reset")
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-    
-   
-    for i in range(20):
-        await RisingEdge(dut.clk)
-        if i % 5 == 0:
-            dut._log.info(f"Simulation cycle {i} in progress...")
 
-    dut._log.info("Test finished successfully without internal signal dependency!")
+    dut._log.info("Test project behavior")
+
+    # Set the input values you want to test
+    dut.ui_in.value = 20
+    dut.uio_in.value = 30
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 50
+
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.

b48f17f52ff9c73822a338e4dc780d80175cc59f
billyhalleck <billyhalleck@gmail.com>
2026-02-12 19:23:00 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,18 +8,18 @@ from cocotb.triggers import Timer, RisingEdge
 async def test_mic1_initial(dut):
     dut._log.info("Starting MIC-1 Basic Test")
 
-    # Saati başlat
+    # Saati baslat
     dut.clk.value = 0
     await Timer(10, units="ns")
     
-    # Reset işlemi
+    # Reset islemi
     dut.rst_n.value = 0
     dut.ena.value = 1
     dut.ui_in.value = 0
     await Timer(20, units="ns")
     dut.rst_n.value = 1
     
-    # Birkaç çevrim bekle ve PC'nin arttığını gözlemle
+    # Birkac cevrim bekle ve PC'nin arttigini gozlemle
     for i in range(10):
         await RisingEdge(dut.clk)
         dut._log.info(f"Cycle {i}: PC is {dut.user_project.PC.value}")
